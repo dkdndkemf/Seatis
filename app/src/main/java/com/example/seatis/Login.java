f418a1664e2a5d9bed9990cc54a4daf52f0ad43c
dkdndkemf <71247865+dkdndkemf@users.noreply.github.com>
2023-04-17 03:58:46 +0900

    카카오 로그인 구현 v.1

--- app/src/main/java/com/example/seatis/Login.java
+++ app/src/main/java/com/example/seatis/Login.java
@@ -3,15 +3,19 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.common.KakaoSdk;
 import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.Account;
 import com.kakao.sdk.user.model.User;
 
 import kotlin.Unit;
@@ -26,53 +30,50 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        Intent intent=new Intent(Login.this,MainActivity.class);
+
         back_btn = findViewById(R.id.back_btn_login);
         kakao_login = findViewById(R.id.kakao_login);
         google_login = findViewById(R.id.google_login);
+        KakaoSdk.init(this, "0ee0994026f39146afc8e662ce6faab8");
 
-        // 카카오가 설치되어 있는지 확인 하는 메서드또한 카카오에서 제공 콜백 객체를 이용함
-        Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
+        back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
-                // 이때 토큰이 전달이 되면 로그인이 성공한 것이고 토큰이 전달되지 않았다면 로그인 실패
-                if (oAuthToken != null) {
-
-                }
-                if (throwable != null) {
-
-                }
-                updateKakaoLoginUi();
-                return null;
+            public void onClick(View v) {
+                finish();
             }
-        };
+        });
         kakao_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (UserApiClient.getInstance().isKakaoTalkLoginAvailable(Login.this)) {
-                    UserApiClient.getInstance().loginWithKakaoTalk(Login.this, callback);
-                    startActivity(intent);
-                } else {
-                    UserApiClient.getInstance().loginWithKakaoAccount(Login.this, callback);
+                if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(Login.this)){
+                    UserApiClient.getInstance().loginWithKakaoTalk(Login.this,(oAuthToken, error) -> {
+                        if (error != null) {
+                            Log.e(TAG, "로그인 실패", error);
+                        } else if (oAuthToken != null) {
+                            Toast.makeText(Login.this, ("로그인 성공(토큰) : " + oAuthToken.getAccessToken()), Toast.LENGTH_SHORT).show();
+                            Intent login_to_main = new Intent(Login.this,MainActivity.class);
+                            ((MainActivity)MainActivity.context_main).main_login_textview.setVisibility(View.GONE);
+                            ((MainActivity)MainActivity.context_main).main_logout_textview.setVisibility(View.VISIBLE);
+                            finish();
+                        }
+                        return null;
+                    });
                 }
+
+                /*
+                UserApiClient.getInstance().me((user, meError) -> {
+                    if (meError != null) {
+                        Log.e(TAG, "사용자 정보 요청 실패", meError);
+                    } else {
+                        Log.i(TAG, "사용자 정보 요청 성공" + "\n이메일: "+user.getKakaoAccount().getEmail());
+                    }
+                    return null;
+                });
+                */
             }
         });
-    }
-    private void updateKakaoLoginUi() {
-        UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
-            @Override
-            public Unit invoke(User user, Throwable throwable) {
-                // 로그인이 되어있으면
-                if (user != null) {
 
-                    // 유저의 아이디
-                    Log.d(TAG, "invoke: id" + user.getId());
-                    // 유저의 어카운트정보에 이메일
-                    Log.d(TAG, "invoke: nickname" + user.getKakaoAccount().getEmail());
-
-                }
-                return null;
-            }
-        });
     }
+
+
 }
--- app/src/main/java/com/example/seatis/MainActivity.java
+++ app/src/main/java/com/example/seatis/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.seatis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,17 +11,17 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button search_button;
-    private TextView main_login_textview, main_logout_textview;
+    public static Context context_main;
+    public TextView main_login_textview, main_logout_textview, search_textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context_main = this;
         Intent main_to_login = new Intent(MainActivity.this, Login.class);
-        search_button = findViewById(R.id.search_button);
+        search_textView = findViewById(R.id.search_button);
         main_login_textview = findViewById(R.id.main_login_textview);
-        main_logout_textview = findViewById(R.id.main_login_textview);
+        main_logout_textview = findViewById(R.id.main_logout_textview);
 
         main_login_textview.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +29,18 @@ public class MainActivity extends AppCompatActivity {
                startActivity(main_to_login);
             }
         });
+        main_logout_textview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 로그아웃 기능
+                main_login_textview.setVisibility(View.VISIBLE);
+                main_logout_textview.setVisibility(View.GONE);
+            }
+        });
+
+
     }
+
 }
+
 
