1b6f5366074a76970bbd25cd4a20d650faec77a6
okokljk <okokljk@gmail.com>
2023-04-16 21:58:51 +0900

    어댑터뷰생성

--- app/src/main/java/com/example/seatis/Detailed_Review.java
+++ app/src/main/java/com/example/seatis/Detailed_Review.java
@@ -1,14 +1,23 @@
 package com.example.seatis;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class Detailed_Review extends Activity {
 
     ListView listView;
     ArrayList<Review> data = new ArrayList<>();
@@ -18,11 +27,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.review);
         data.add(new Review("안강현", "2023.12.22", 2, 3, 4, "여기서는 잘보이지 않고 소리도 별로에요", 12, 2));
-
+        data.add(new Review("아아아", "2023.11.22", 5, 5, 4, "좋아요", 1, 123));
 
         Detailed_Review_Adapter detailed_review_adapter = new Detailed_Review_Adapter(data);
 
         listView = findViewById(R.id.review_viewer);
         listView.setAdapter(detailed_review_adapter);
     }
+
 }
+
