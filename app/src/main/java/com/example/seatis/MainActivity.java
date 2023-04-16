0630d43f936175eb0fc47a98606236ac99d1ec9e
okokljk <okokljk@gmail.com>
2023-04-16 22:01:34 +0900

    어댑터뷰생성

--- app/src/main/java/com/example/seatis/MainActivity.java
+++ app/src/main/java/com/example/seatis/MainActivity.java
@@ -1,28 +1,14 @@
 package com.example.seatis;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
-    ArrayList<Review> data = new ArrayList<>();
-
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.review);
-        data.add(new Review("안강현", "2023.12.22", 2, 3, 4, "여기서는 잘보이지 않고 소리도 별로에요", 12, 2));
-
-
-        Detailed_Review_Adapter detailed_review_adapter = new Detailed_Review_Adapter(data);
-
-        listView = findViewById(R.id.review_viewer);
-        listView.setAdapter(detailed_review_adapter);
+        setContentView(R.layout.activity_main);
     }
 }
