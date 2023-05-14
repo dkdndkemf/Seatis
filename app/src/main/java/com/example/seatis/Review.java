159b788e8cae42a3c4ac84cb3d39398fedf4d6d3
okokljk <okokljk@gmail.com>
2023-05-14 15:58:15 +0900

    이미지url

--- app/src/main/java/com/example/seatis/Review.java
+++ app/src/main/java/com/example/seatis/Review.java
@@ -11,7 +11,19 @@ public class Review {
     String review;
     int agree;
     int disagree;
+    String img_url;
 
+    public Review(String name, String date, float see_rating, float listen_rating, float etc_rating, String review, int agree, int disagree,String img_url) {
+        this.name = name;
+        this.date = date;
+        this.see_rating = see_rating;
+        this.listen_rating = listen_rating;
+        this.etc_rating = etc_rating;
+        this.review = review;
+        this.agree = agree;
+        this.disagree = disagree;
+        this.img_url=img_url;
+    }//이미지가 있을때 생성자
     public Review(String name, String date, float see_rating, float listen_rating, float etc_rating, String review, int agree, int disagree) {
         this.name = name;
         this.date = date;
@@ -21,7 +33,7 @@ public class Review {
         this.review = review;
         this.agree = agree;
         this.disagree = disagree;
-    }
+    }//이미지가 없을때 생성자
 
     public String getName() {
         return name;
@@ -87,5 +99,12 @@ public class Review {
         this.disagree = disagree;
     }
 
+    public String getImg_Url() {
+        return img_url;
+    }
+
+    public void setImg_Url(String img_url) {
+        this.img_url = img_url;
+    }
 
 }
