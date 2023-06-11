04dccfcb5f138d79ac17bf9711a0f29e08c21cf5
JeonggyoLee <jkl07931728@gmail.com>
2023-06-12 03:28:23 +0900

    카메라,갤러리 선택해서 이미지뷰에 반영 성공

--- app/src/main/java/com/example/seatis/F_ReviewWrite.java
+++ app/src/main/java/com/example/seatis/F_ReviewWrite.java
@@ -2,14 +2,19 @@ package com.example.seatis;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -18,20 +23,19 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.zip.Inflater;
+import java.util.Date;
+import java.util.Locale;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link F_ReviewWrite#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class F_ReviewWrite extends Fragment {
 
     ListView listView;
@@ -41,6 +45,9 @@ public class F_ReviewWrite extends Fragment {
     Button write_btn;
     EditText write_review;
 
+    Button photo_select; //사진 선택부분
+    ImageView photoImageView; //이미지 반영부분
+
     TextView no_review;
     RatingBar see_score;
     RatingBar listen_score;
@@ -48,28 +55,18 @@ public class F_ReviewWrite extends Fragment {
 
     ConstraintLayout layout;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private File photoFile;
 
     public F_ReviewWrite() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment F_ReviewWrite.
-     */
-    // TODO: Rename and change types and number of parameters
     public static F_ReviewWrite newInstance(String param1, String param2) {
         F_ReviewWrite fragment = new F_ReviewWrite();
         Bundle args = new Bundle();
@@ -91,43 +88,37 @@ public class F_ReviewWrite extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_f__review_write, container, false);
         Activity activity = getActivity();
+        layout = (ConstraintLayout) view.findViewById(R.id.layout);
 
-        layout = (ConstraintLayout)view.findViewById(R.id.layout);
+        back_btn = (ImageButton) view.findViewById(R.id.back_Btn);
+        write_btn = (Button) view.findViewById(R.id.write_btn);
+        write_review = (EditText) view.findViewById(R.id.write_review);
+        photo_select = (Button) view.findViewById(R.id.photo_select);
+        photoImageView = (ImageView) view.findViewById(R.id.photoImageView);
 
-        back_btn = (ImageButton)view.findViewById(R.id.back_Btn);
-        write_btn = (Button)view.findViewById(R.id.write_btn);
-        write_review = (EditText)view.findViewById(R.id.write_review);
-
-        see_score = (RatingBar)view.findViewById(R.id.see_score);
-        listen_score = (RatingBar)view.findViewById(R.id.listen_score);
-        etc_score = (RatingBar)view.findViewById(R.id.etc_score);
-        //data = ((F_DetailedReview)F_DetailedReview.context_DetailedReview).data;
+        see_score = (RatingBar) view.findViewById(R.id.see_score);
+        listen_score = (RatingBar) view.findViewById(R.id.listen_score);
+        etc_score = (RatingBar) view.findViewById(R.id.etc_score);
         data = F_DetailedReview.data;
-        //F_DetailedReview_adapter = ((F_DetailedReview)F_DetailedReview.context_DetailedReview).F_DetailedReview_adapter;
         F_DetailedReview_adapter = F_DetailedReview.detailed_review_adapter;
-        listView = (ListView)activity.findViewById(R.id.review_viewer);
-        no_review = (TextView)activity.findViewById(R.id.no_review);
+        listView = (ListView) activity.findViewById(R.id.review_viewer);
+        no_review = (TextView) activity.findViewById(R.id.no_review);
 
         FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
         InputMethodManager imm = (InputMethodManager) getActivity().getApplicationContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
 
-       layout.setOnTouchListener(new View.OnTouchListener() {
-           @Override
-           public boolean onTouch(View v, MotionEvent event) {
-               if (getActivity() != null && getActivity().getCurrentFocus() != null)
-               {
-                   // 프래그먼트기 때문에 getActivity() 사용
-                   //InputMethodManager inputManager = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-                   imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-               }
-               return false;
-           }
-       });
+        layout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (getActivity() != null && getActivity().getCurrentFocus() != null) {
+                    imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+                return false;
+            }
+        });
 
-        // 뒤로가기
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -137,19 +128,15 @@ public class F_ReviewWrite extends Fragment {
             }
         });
 
-        // 리뷰등록
         write_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                data.add(new Review("테스트","2023.04.21",see_score.getRating(),listen_score.getRating(), etc_score.getRating(),write_review.getText().toString(),0,0 ));
-                if(data.isEmpty())
-                {
+                data.add(new Review("테스트", "2023.04.21", see_score.getRating(), listen_score.getRating(), etc_score.getRating(), write_review.getText().toString(), 0, 0));
+                if (data.isEmpty()) {
                     listView.setVisibility(View.INVISIBLE);
                     no_review.setVisibility(View.VISIBLE);
                     F_DetailedReview_adapter.notifyDataSetChanged();
-                }
-                else
-                {
+                } else {
                     listView.setVisibility(View.VISIBLE);
                     no_review.setVisibility(View.GONE);
                     F_DetailedReview_adapter.notifyDataSetChanged();
@@ -159,6 +146,80 @@ public class F_ReviewWrite extends Fragment {
                 fragmentManager.popBackStack();
             }
         });
+
+        photo_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                galleryIntent.setType("image/*");
+
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (cameraIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+
+                    if (photoFile != null) {
+                        Uri photoUri = FileProvider.getUriForFile(getActivity(), "com.example.seatis.fileprovider", photoFile);
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                    }
+                }
+
+                Intent chooserIntent = Intent.createChooser(galleryIntent, "Select Image");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] { cameraIntent });
+
+                startActivityForResult(chooserIntent, 1);
+            }
+        });
+
         return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            if (photoFile != null) {
+                Bitmap bitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                photoImageView.setImageBitmap(bitmap);
+            }
+
+            // 이미지뷰에 갤러리에서 선택한 이미지 세팅
+            if (data != null) {
+                Uri selectedImageUri = data.getData();
+                if (selectedImageUri != null) {
+                    try {
+                        InputStream inputStream = getActivity().getContentResolver().openInputStream(selectedImageUri);
+                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                        if (bitmap != null) {
+                            photoImageView.setImageBitmap(bitmap);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(null);
+        File image = null;
+        try {
+            image = File.createTempFile(
+                    imageFileName,
+                    ".jpg",
+                    storageDir
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
 }
