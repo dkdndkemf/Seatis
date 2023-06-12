eb2703a0b8b2ab7b72962bd03c2884695d431ace
kanghyeonn <100706004+kanghyeonn@users.noreply.github.com>
2023-06-12 18:58:11 +0900

    리뷰 등록

--- app/src/main/java/com/example/seatis/GetSeatId.java
+++ app/src/main/java/com/example/seatis/GetSeatId.java
@@ -8,14 +8,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GetSeatId extends StringRequest {
-    final static private String URL = "http://101.101.211.66:8080/SeatIs/GetSeatId.jsp";
+    final static private String URL = "http://101.101.211.66:8080/SeatIs/GetUS.jsp";
     private Map<String, String> parameters;
-    public GetSeatId(String theaterName, String seatCol, String seatRow, Response.Listener rListener) {
+    public GetSeatId(String user_email, int theaterid, String seatCol, int seatRow, Response.Listener rListener) {
         super(Method.POST, URL, rListener, null);
         parameters = new HashMap<>();
-        parameters.put("theaterName", theaterName);
+        parameters.put("user_email", user_email);
+        parameters.put("theater_id", String.valueOf(theaterid));
         parameters.put("seatCol", seatCol);
-        parameters.put("seatRow", seatRow);
+        parameters.put("seatRow", String.valueOf(seatRow));
     }
     protected Map<String, String> getParams() throws AuthFailureError {
         return parameters;
